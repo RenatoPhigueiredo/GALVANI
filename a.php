0739f242c2aed9f5ac8270627edf3dabbc163b21
Lab05_etec <Lab05_etec@etec.com>
2025-06-04 16:23:17 -0300

    imobiliaria

--- a.php
+++ a.php
@@ -10,18 +10,36 @@
     $Tipo=$_POST["Tipo"];
     $Valor=$_POST["Valor"];
 
-    $con=mysqli_connect("localhost","root","","imobiliaria");
+    $con=mysqli_connect("localhost","root","","imobiliaria") or die("erro!");
 
-    $in="insert value imobiliaria(null,
-                                    '$Endereco',
-                                    '$Cidade',
-                                    '$Estado',
-                                    '$CEP',
-                                    '$Tipo',
-                                    '$Valor')";
+    $in= "insert into propriedades values (null,
+                                        '$Endereco',
+                                        '$Cidade',
+                                        '$Estado',
+                                        '$CEP',
+                                        '$Tipo',
+                                        '$Valor')";
                             
-    $incluir=mysli_query($con,$in);
-    if($)
+    $incluir=mysqli_query($con, $in);
+
+    if($incluir == 1)
+    {
+        echo"
+        Endereço:$Endereco<br>
+        Cidade: $Cidade<Br>
+        Estado: $Estado<br>
+        CEP: $CEP<br>
+        Tipo: $Tipo<br>
+        Valor: $Valor<br>
+        ";
+        echo "foi incluido com sucesso";
+    }
+    else
+    {
+        echo "Nao foi possivel concluir esta ação <br>";
+
+        echo "aaaaaaaaaaaaaaaaaa vo curinga";
+    }
     ?>
 </body>
 </html>
