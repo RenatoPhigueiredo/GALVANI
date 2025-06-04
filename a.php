4b298189c6005754e60b06cf78507f408ed4b94c
Lab05_etec <Lab05_etec@etec.com>
2025-06-04 15:28:20 -0300

    fazer

---
+++ a.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+    $IdPropriedade=$_POST["IdPropriedade"];
+    $Endereco=$_POST["Endereco"];
+    $Cidade=$_POST["Cidade"];
+    $Estado=$_POST["Estado"];
+    $CEP=$_POST["CEP"];
+    $Tipo=$_POST["Tipo"];
+    $Valor=$_POST["Valor"];
+
+    $con=mysqli_connect("localhost","root","","imobiliaria");
+
+    $in="insert value imobiliaria(null,
+                                    '$Endereco',
+                                    '$Cidade',
+                                    '$Estado',
+                                    '$CEP',
+                                    '$Tipo',
+                                    '$Valor')";
+                            
+    $incluir=mysli_query($con,$in);
+    ?>
+</body>
+</html>
